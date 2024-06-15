1e7038fa3457f9d2249f92b0a39983e33d8a5518
youta <youtatu@163.com>
2024-06-15 17:19:41 +0800

    加了内容

---
+++ xiaomi-service/src/main/java/Student.java
@@ -0,0 +1,13 @@
+import java.security.PrivateKey;
+
+/**
+ * @author youta
+ * @version v1.0
+ * @project xiaomi-parent
+ * @package PACKAGE_NAME
+ * @company 千锋教育
+ * @date 2024/6/15 17:17
+ */
+public class Student {
+    PrivateKey privateKey;
+}
---
+++ xiaomi-service/src/main/java/com/dev.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ * @author youta
+ * @version v1.0
+ * @project xiaomi-parent
+ * @package com
+ * @company 千锋教育
+ * @date 2024/6/15 17:18
+ */
+public class dev {
+    public static void main(String[] args) {
+
+    }
+}
